9210af95f4110699cd8cb7fdcbc5465309d3caf8
buxiangqi_mac <3160231312@qq.com>
2025-05-02 22:31:57 +0800

    新增根据id查询课程接口， 新增更新课程信息接口

--- xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/service/CourseBaseInfoService.java
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/service/CourseBaseInfoService.java
@@ -4,10 +4,13 @@ import com.xuecheng.base.model.PageParams;
 import com.xuecheng.base.model.PageResult;
 import com.xuecheng.content.model.dto.AddCourseDto;
 import com.xuecheng.content.model.dto.CourseBaseInfoDto;
+import com.xuecheng.content.model.dto.EditCourseDto;
 import com.xuecheng.content.model.dto.QueryCourseParamsDto;
 import com.xuecheng.content.model.po.CourseBase;
 
 public interface CourseBaseInfoService {
     public PageResult<CourseBase> queryCourseBaseList(PageParams pageParams, QueryCourseParamsDto queryCourseParamsDto);
     public CourseBaseInfoDto createCourseBase(Long companyId, AddCourseDto addCourseDto);
+    public CourseBaseInfoDto getCourseBaseInfo(long courseId);
+    public CourseBaseInfoDto updateCourseBase(Long companyId, EditCourseDto dto);
 }
